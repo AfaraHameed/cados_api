ed8058d3602f413bb35bfb96a0bf16597533fb02
AfaraHameed <afarasidheek@gmail.com>
2023-05-04 16:44:48 +0530

    authentication with jwt added

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns=[
     path('', views.endpoints,name='endpoints'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('advocates/',views.advocates_list,name='advocates'),
     # path('advocates/<str:username>/',views.advocates_details,name='advocates_details'),
     path('advocates/<str:username>/',views.AdvocateDetail.as_view()),
--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from . models import Advocates
 from . serializers import AdvocateSerializer,CompanySerializer
@@ -17,12 +17,14 @@ from rest_framework.permissions import IsAuthenticated
 # PUT/advocates/:id
 # DELETE/advocates/:id
 
-@api_view(['GET','POST'])
+
+@api_view(['GET'])
 def endpoints(request):
     data=['/advocates','advocates/:username']
     return Response(data)
 
 @api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
 def  advocates_list(request):
     if request.method == 'GET':
         query=request.GET.get('query')
